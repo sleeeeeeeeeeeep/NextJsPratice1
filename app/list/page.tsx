455339d616715ade9489f3b87a7dc77cc735a5f1
meureuet <gmdgjfr@gmail.com>
2023-04-15 18:00:26 +0900

    map() 사용

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,8 +1,13 @@
+import Image, { StaticImageData } from "next/image"
+import ImgEggtart from '/public/에그타르트.jpg'
+import ImgTiramisu from '/public/티라미수.jpg'
+import ImgCheeseCake from '/public/치즈케이크.jpg'
 import Link from "next/link"
 
 export default function List() {
   const subListLink: string = '/list/subList'
-
+  
+  let imgPath: StaticImageData[] = [ImgEggtart, ImgTiramisu, ImgCheeseCake]
   let goods: string[] = ['에그타르트', '티라미수', '치즈케이크']
 
   return (
@@ -11,9 +16,16 @@ export default function List() {
         <h4 className="title">목록</h4>
       </div>
 
-      <div>
-        <h4 className="food">에그타르트</h4>
-      </div>
+      {
+        goods.map((good, index) => {
+          return (
+            <div className="goods" key={index}>
+              <Image src={imgPath[index]} className="goods-img" alt="이미지"/>
+              <h4 >{good}</h4>
+            </div>
+          )
+        })
+      }
 
       <Link href={subListLink}>링크</Link>
     </div>
