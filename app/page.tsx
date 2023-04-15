825b83abf2c9a6b7910cc8c8f05187e50f290904
meureuet <gmdgjfr@gmail.com>
2023-04-15 16:18:04 +0900

    찍먹

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,13 @@ import styles from './page.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  let strWow: string = '와우'
+  const link: string = 'http://google.com'
   return (
-    <main>
-      
-    </main>
+    <div>
+      <h4 className='title'>메인</h4>
+      <p className='sub-title'>NextJS 찍먹 {strWow}</p>
+      <a href={link}>구글 링크</a>
+    </div>
   )
 }
