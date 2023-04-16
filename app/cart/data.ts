545d47124d07db3b727089c464668ae75e4cc5b7
meureuet <gmdgjfr@gmail.com>
2023-04-16 18:24:12 +0900

    컴포넌트

---
+++ app/cart/data.ts
@@ -0,0 +1,4 @@
+let num: number = 123;
+let str: string = '가나다';
+
+export {num, str};
--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,17 +1,41 @@
+'use client'
+import { num, str } from './data'
+
 export default function Cart() {
-    return (
-      <div>
-        <h4 className="title">Cart</h4>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
-      </div>
-    )
-  } 
+  let cartGoods: string[] = ['에그타르트', '티라미수', '치즈케이크']
+
+  return (
+    <div>
+      <h4 className="title">Cart</h4>
+      <p>{num}</p>
+      <p>{str}</p>
+
+      {
+        cartGoods.map((item, index) => {
+          return (
+            <CartItem item={item} />
+          )
+        })
+      }
+
+      <Banner content="123" />
+
+    </div>
+  )
+}
+
+function Banner(props: any) {
+  return (
+    <h5>배너{props.content}</h5>
+  )
+}
+
+function CartItem(props: any) {
+  return (
+    <div className="cart-item">
+      <p>{props.item}</p>
+      <p>$40</p>
+      <p>1개</p>
+    </div>
+  )
+}
